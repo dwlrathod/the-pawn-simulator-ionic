62d392052d72bfe33c4577472276325b923351bc
Dhawal Rathod <rathoddhawal23@gmail.com>
2022-03-27 22:45:19 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'the-pawn-simulator',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
